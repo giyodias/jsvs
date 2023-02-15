275f400a2f9389729d3a82fe93c399c802d43a41
Giyo Santana Dias <giosantdias@gmail.com>
2023-02-15 19:17:02 -0300

    Exercício um de Array

---
+++ exDoisArray/src/exDoisArray/DoisArray.java
@@ -0,0 +1,14 @@
+package exDoisArray;
+
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DoisArray {
+
+	public static void main(String[] args) {
+		Set<Integer> numeros = new HashSet <Integer>(); //nomeando o set queé integer igual faz o scanner
+
+	}
+
+}
